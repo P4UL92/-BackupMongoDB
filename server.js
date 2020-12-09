c37a23a01a3b3bc62dca92576c0c3b853f528cba
Eder <ederlarico1@gmail.com>
2020-12-09 11:05:07 -0500

    second

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const shell = require('shelljs')
 const app = express();
+const archiver = require('archiver');
 
 app.use(express.urlencoded({ extended: true }));
 
@@ -8,12 +9,31 @@ app.use(express.urlencoded({ extended: true }));
 app.get('/', function() {
   
 
+function zipDirectory(source, out) {
+  const archive = archiver('zip', { zlib: { level: 9 }});
+  const stream = fs.createWriteStream(out);
+
+  return new Promise((resolve, reject) => {
+    archive
+      .directory(source, false)
+      .on('error', err => reject(err))
+      .pipe(stream)
+    ;
+
+    stream.on('close', () => resolve());
+    archive.finalize();
+  });
+}
 
 });
 
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 app.listen(3000, "127.0.0.1", function() {
-shell.exec('echo ok')
+	var backupM = function backupMongoDB() {
+		shell.exec('mongodump --host=127.0.0.1 --port=27017 --out=D:/backups/');
+		zipDirectory('D:/backups/', 'D:/backups.zip');
+	}
+	setInterval(backupM, 1000 * 15);
 });
