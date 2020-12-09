0341bd6f665ed54b79625a6c2bbd8c6d3c7634fe
unknown <cedyvq@gmail.com>
2020-12-09 11:11:57 -0600

    7z-min

--- server.js
+++ server.js
@@ -3,15 +3,13 @@ const shell = require('shelljs')
 const fs = require('fs')
 const app = express();
 const archiver = require('archiver');
+const _7z = require('7zip-min');
 
 app.use(express.urlencoded({ extended: true }));
 
 
 app.get('/', function() {
-  
-
-
-
+ 
 });
 
 
@@ -22,9 +20,12 @@ app.listen(3000, "127.0.0.1", function() {
 	
 
 	var backupM = function backupMongoDB() {
-		shell.exec('mongodump --host=127.0.0.1 --port=27017 --out=D:/backups/');
+		shell.exec('mongodump --host=10.0.0.122 -u=dbadmin -p=dbadmin108 --port=27017 --out=D:/backups/');
 
-  const archive = archiver('zip', { zlib: { level: 9 }});
+    let pack = _7z.pack('D:/backups/', 'D:/backups.7z', err => {
+      console.log('I am done with compressing')
+  });
+  /*const archive = archiver('zip', { zlib: { level: 9 }});
   const stream = fs.createWriteStream('D:/backups.zip');
 
   return new Promise((resolve, reject) => {
@@ -37,7 +38,7 @@ app.listen(3000, "127.0.0.1", function() {
     stream.on('close', () => resolve());
     archive.finalize();
   });
-
+*/
 	};
 	setInterval(backupM, 1000 * 15);
 });
